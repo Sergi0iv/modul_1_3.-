256ccdb37382bb68d6bf170101b62d66fe55adb2
Sergi0iv <rickitir2.0@gmail.com>
2024-08-02 17:29:14 +0300

    Add files via upload

---
+++ modul_1_3.py
@@ -0,0 +1,8 @@
+name = 'Сергей'
+print('Меня завут ',name,',',sep='')
+age = 25
+print('мне',age,'лет')
+age = 18
+print('выгляжу на ',age,'.',sep='')
+is_student = True
+print('Всё пере',is_student,' =)',sep='')
